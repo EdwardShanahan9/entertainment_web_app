696e3294ffded5fa5a4ab2646b68145c61ee0a01
EdwardShanahan9 <193254185+EdwardShanahan9@users.noreply.github.com>
2025-03-05 15:09:40 +0000

    Add custom color and font

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,9 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <h1 className="text-blue-400 text-7xl">Hello World</h1>
+      <h1 className="">Hello World</h1>
     </>
   );
 }
