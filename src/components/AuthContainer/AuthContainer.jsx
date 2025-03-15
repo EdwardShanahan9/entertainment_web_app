14769d3131c1029c411887fdce461b0e173005f1
EdwardShanahan9 <193254185+EdwardShanahan9@users.noreply.github.com>
2025-03-15 12:17:43 +0000

    Add error message if fields are empty, note functionality is'nt coded yet.

--- src/components/AuthContainer/AuthContainer.jsx
+++ src/components/AuthContainer/AuthContainer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const AuthContainer = ({ children, title }) => {
   return (
-    <div className="p-6 bg-dark-blue rounded-10 w-[327px] md:w-[400px]">
+    <div className="p-6 bg-dark-blue rounded-10 w-[327px] md:w-[400px] ">
       <h2 className="text-white text-3xl mb-10">{title}</h2>
       {children}
     </div>
--- src/layout/AuthLayout/Register/Register.jsx
+++ src/layout/AuthLayout/Register/Register.jsx
@@ -1,25 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router";
 import AuthContainer from "../../../components/AuthContainer/AuthContainer";
 import Input from "../../../components/Input/Input";
 import Button from "../../../components/Button/Button";
 
 const Register = () => {
+  const [errorMessage, setErrorMessage] = useState(false);
+
   return (
     <AuthContainer title="Sign Up">
       <form>
-        <Input type="email" placeholder="Email Address" />
+        <div className="relative">
+          <Input type="email" placeholder="Email Address" />
 
-        <Input type="password" placeholder="Password" />
+          {errorMessage ? (
+            <span className="text-sm text-primary absolute right-4 top-0 font-light">
+              Can’t be empty
+            </span>
+          ) : (
+            ""
+          )}
+        </div>
 
-        <Input type="password" placeholder="Repeat Password" />
+        <div className="relative">
+          <Input type="password" placeholder="Password" />
+
+          {errorMessage ? (
+            <span className="text-sm text-primary absolute right-4 top-0 font-light">
+              Can’t be empty
+            </span>
+          ) : (
+            ""
+          )}
+        </div>
+
+        <div className="relative">
+          <Input type="password" placeholder="Repeat Password" />
+
+          {errorMessage ? (
+            <span className="text-sm text-primary absolute right-4 top-0 font-light">
+              Can’t be empty
+            </span>
+          ) : (
+            ""
+          )}
+        </div>
 
         <Button text="Create an account" />
       </form>
 
       <p className="text-[15px] text-white text-center">
         Alreadt have an account?{" "}
-        <Link className="text-primary" to="/login">
+        <Link className="text-primary ml-2" to="/login">
           Login
         </Link>
       </p>
